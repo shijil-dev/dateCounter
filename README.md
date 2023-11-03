7a5515ccd7defc14372aba5abd3c38342f6d18fa
dave <shijildave@gmail.com>
2023-11-03 12:42:13 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+n## Available Scripts
+
+In the project directory, you can run:
+
+### `npm test`
+
+Runs the app in the development mode.
+Open [http://localhost:1234](http://localhost:1234) to view it in your browser.
+
+The page will reload when you make changes.
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import React from "react";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
